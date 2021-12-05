ab8b5f6556dc011098fc065eba46ce4cb74627dd
natarajanworld <94781463+natarajanworld@users.noreply.github.com>
2021-12-05 20:24:46 +0530

    Update example5.py

--- example5.py
+++ example5.py
@@ -1,3 +1,7 @@
+# A Pandas Series is like a column in a table.
+
+# It is a one-dimensional array holding data of any type.
+
 import pandas as pd
 
 calories = {"day1": 420, "day2": 380, "day3": 390}
