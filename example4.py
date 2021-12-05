eeb12d5b930ddd9739132de77e972b55177318de
natarajanworld <94781463+natarajanworld@users.noreply.github.com>
2021-12-05 20:24:13 +0530

    Update example4.py

--- example4.py
+++ example4.py
@@ -1,3 +1,11 @@
+# Pandas is a Python library.
+ 
+# Pandas is used to analyze data.
+
+# A Pandas Series is like a column in a table.
+
+# It is a one-dimensional array holding data of any type.
+
 import pandas as pd
 
 calories = {"day1": 420, "day2": 380, "day3": 390}
