a26f072595e500a75e6db9274cd13426f1e9ef4c
natarajanworld <94781463+natarajanworld@users.noreply.github.com>
2021-12-05 20:20:48 +0530

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,3 +1,9 @@
+#Machine Learning is making the computer learn from studying data and statistics.
+
+#Machine Learning is a step into the direction of artificial intelligence (AI).
+
+#Machine Learning is a program that analyses data and learns to predict the outcome.
+
 import numpy
 
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
@@ -10,3 +16,4 @@ print(x)
 #Mean - The average value
 #Median - The mid point value
 #Mode - The most common value
+
